3cbdfde91297f4f0690a60f07e58d1f2cb3cbfe3
Jacob Jones <101999077+yosheDev@users.noreply.github.com>
2026-07-02 13:50:41 -0500

    Project created

---
+++ Source/RoundsLikePrototype/RoundsLikePrototype.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RoundsLikePrototype.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RoundsLikePrototype, "RoundsLikePrototype" );
+
+DEFINE_LOG_CATEGORY(LogRoundsLikePrototype)
---
+++ Source/RoundsLikePrototype/RoundsLikePrototypeGameMode.h
@@ -0,0 +1,22 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RoundsLikePrototypeGameMode.generated.h"
+
+/**
+ *  Simple GameMode for a first person game
+ */
+UCLASS(abstract)
+class ARoundsLikePrototypeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARoundsLikePrototypeGameMode();
+};
+
+
+
